ff23d981abea0509f06267090975640fdf9123c8
ms2010gamer <msoheib2010@gmail.com>
2023-03-11 16:19:42 +0100

    changed the target and obj directories

--- premake5.lua
+++ premake5.lua
@@ -11,12 +11,18 @@ workspace "cobra"
     filter "configurations:Release"
         defines { "NDEBUG" }
         optimize "On"
+    
+tdir = "bin/%{cfg.buildcfg}/%{prj.name}"
+odir = "bin-obj/%{cfg.buildcfg}/%{prj.name}"
 
 project "cobraeditor"
     location "cobraeditor"
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++17"
+
+    targetdir(tdir)
+    objdir(odir)
 
     files
     {
