70e2870689daf9a05e8e05a7dacc60cda342bb4a
ms2010gamer <msoheib2010@gmail.com>
2023-03-11 17:52:06 +0100

    another lil upgrade

--- cli.py
+++ cli.py
@@ -12,6 +12,7 @@ TOOLS_DIR = "tools"
 def RunCommand(cmd):
     script = f"{os.curdir}/{TOOLS_DIR}/{cmd}.py"
     if os.path.exists(script):
+        print("Excuting: ", cmd)
         subprocess.call(["python3", ])
     else:
         print("Invalid command: ", cmd)
@@ -20,6 +21,5 @@ for i in range(1, len(sys.argv)):
     cmd = sys.argv[1]
 
     print("\n----------------------------------")
-    print("Excuting: ", cmd)
 
     RunCommand(cmd)
